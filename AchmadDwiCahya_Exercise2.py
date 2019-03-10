95ac5976ed7568ef5a9e3a7243ce5a9ebf8fae0d
unknown <achmaddwicahya8@gmail.com>
2019-03-10 14:00:54 +0700

    Exercise 1 & 2

---
+++ AchmadDwiCahya_Exercise2.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+strip = ("---------------")
+print (strip)
+X = ["A | B | C | D"],["E | F | G | H"],["I | J | K | L"],["M | N | O | P"]
+strip = ("---------------")
+for i in [0,1,2,3]:
+    print (X[i][0])
+    print (strip)
