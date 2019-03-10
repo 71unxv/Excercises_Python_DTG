d29dca9f012ea72d06469fafba6d875eb9f6e8c6
almarsa terakahfi <almarsa.trkhf@gmail.com>
2019-03-10 13:58:21 +0700

    .\.git\

---
+++ ALMARSA_2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 13:33:19 2019
+
+@author: Almarsa
+"""
+
+nested = [['A','B','C','D'],['E','F','G','H'],['I','J','K','L'],['M','N','O','P']]
+a = nested[0]
+b = nested[1]
+c = nested[2]
+d = nested[-1]
+e = "---------------"
+print(e)
+print(a[0],"|",a[1],"|",a[2],"|",a[-1],"|")
+print(e)
+print(b[0],"|",b[1],"|",b[2],"|",b[-1],"|")
+print(e)
+print(c[0],"|",c[1],"|",c[2],"|",c[-1],"|")
+print(e)
+print(d[0],"|",d[1],"|",d[2],"|",d[-1],"|")
+print(e)
