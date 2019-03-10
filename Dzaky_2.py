403242f811f397b12a36dc718fc47317cf375c76
Dzaky <reyhandzaky.rd@gmail.com>
2019-03-10 14:03:24 +0700

    Dzaky_2.py

---
+++ Dzaky_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 13:23:02 2019
+
+@author: gayuh
+"""
+garis='--------------'
+aw=['A | B | C | D','E | F | G | H','I | J | K | L','M | N | O | P']
+print(garis)
+print(aw[0])
+print(garis)
+print(aw[1])
+print(garis)
+print(aw[2])
+print(garis)
+print(aw[3])
+print(garis)
