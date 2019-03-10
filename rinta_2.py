05699b9ceaa6a5bf01995a93ab8e1ebecf04f354
rintabt <rintabt@gmail.com>
2019-03-10 13:54:55 +0700

    ver_02

--- rinta_2.py
+++ rinta_2.py
@@ -8,11 +8,11 @@ strip='---------------'
 var = [['A | B | C | D'],['E | F | G | H'],['I | J | K | L'],['M | N | O | P']]
 
 print(strip)
-print(f"{var[0]}")
+print(f"{var[0][0]}")
 print(strip)
-print(f"{var[1]}")
+print(f"{var[1][0]}")
 print(strip)
-print(f"{var[2]}")
+print(f"{var[2][0]}")
 print(strip)
-print(f"{var[3]}")
+print(f"{var[3][0]}")
 print(strip)
