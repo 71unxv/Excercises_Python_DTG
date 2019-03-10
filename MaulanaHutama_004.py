29d5efa78b34ea3e0b657a99191fa1e5b9e2d26d
Maulana Hutama <maulana.hutama14@outlook.com>
2019-03-10 15:31:09 +0700

    Pengecek Bilangan Prima

---
+++ MaulanaHutama_004.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def PrintPrime(A):
+    for item in A:
+        isi = 0
+        for i in range(1,item+1):
+            if item % i == 0:
+                isi += 1
+        if isi == 2:
+            print('Bilangan Prima')
+        elif isi == 1:
+            print('Bilangan Prima')
+        else:
+            print('Bukangan Bilangan Prima')
+        isi = 0
+        
+
+PrintPrime([1,2,3,4,5,6,7,8,9,10])
