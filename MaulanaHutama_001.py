629f64bd7ca5d7aeab97375fa28100c58fdfbf2a
Maulana Hutama <maulana.hutama14@outlook.com>
2019-03-10 13:06:40 +0700

    'Maulana_01'

---
+++ MaulanaHutama_001.py
@@ -0,0 +1,16 @@
+Jhon = 'Jhon'
+JApple = 8
+JPrice = 100.00
+Dilan = 'Dilan'
+DApple = 6
+DPrice = 75.00
+Sarah = 'Sarah'
+SApple = 12
+SPrice = 150.00
+apple = 1
+aPrice = 12.5
+
+print(Jhon+f' have {JApple} apple and the price is {JPrice}')
+print(Dilan+f' have {DApple} apple and the price is {DPrice}')
+print(Sarah+f' have {SApple} apple and the price is {SPrice}')
+print(f'and the price for {apple} apple is {aPrice}')
