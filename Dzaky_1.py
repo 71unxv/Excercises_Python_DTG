a2ae0d7e0c81e2cfb0d862fa0644707f2da39395
Dzaky <reyhandzaky.rd@gmail.com>
2019-03-10 13:07:09 +0700

    Dzaky_1

---
+++ Dzaky_1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 12:52:36 2019
+
+@author: gayuh
+"""
+
+a = 12.50
+john = 8
+dilan = 6
+sarah = 12
+
+print ("John have 8 apple and the price is", john*a)
+print ("Dilan have 6 apple and the price is", dilan*a)
+print ("Sarah have 12 apple and the price is", sarah*a)
+print ("And the price for 1 apple is",a)
