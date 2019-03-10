b5eef3cdfb79c4a9f110627704e270b85f37dc11
almarsa terakahfi <almarsa.trkhf@gmail.com>
2019-03-10 13:03:21 +0700

    Almarsa_1.py

---
+++ Almarsa_1.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 12:50:30 2019
+
+@author: Almarsa
+"""
+a = 8
+b = 6 
+c = 12
+d = 1
+ai = 100.00
+bj = 75.00
+ck = 150.00
+dl = 12.50
+
+print(f"Jhon have {a} apple and the price is {ai}")
+print(f"Dilan have {b} apple and the price is {bj}")
+print(f'Sarah have {c} apple and the price is {ck}')
+print(f'And the price for {d} apple is {dl}')
+
+
