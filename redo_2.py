d72e38a3c4240f939c63ade34a1f18adf9d7412d
Redo Faris Novrizki <redofaris@gmail.com>
2019-03-10 14:12:27 +0700

    redo_2

---
+++ redo_2.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 13:34:40 2019
+
+@author: redof
+"""
+
+strip="-------------"
+nested = [["A | B | C | D"],["E | F | G | H "],["I | J | K | L"],["M | N | O |P"]]
+for i in [0,1,2,3]:
+    print(strip)
+    print(nested[i][0])
+    print(strip)
+  
