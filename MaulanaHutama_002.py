fc07776f4e5bb42fb24bf6411d1dd666d0a8198a
Maulana Hutama <maulana.hutama14@outlook.com>
2019-03-10 13:50:13 +0700

    Excercise 2

---
+++ MaulanaHutama_002.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+strip = '--------------'
+A = [['A | B | C | D |'],['D | E | F | G |'],
+     ['H | I | J | K |'],['L | M | N | O |']]
+
+for i in [0,1,2,3]:
+    print(strip)
+    print(A[i][0])
+
+print(strip)    
+#print(strip)
+#print(A[0][0])
+#print(strip)
+#print(A[1][0])
+#print(strip)
+#print(A[2][0])
+#print(strip)
+#print(A[3][0])
+#print(strip)
