992e97736f238a81efa56c1b8bb5ec96d90920f9
Redo Faris Novrizki <redofaris@gmail.com>
2019-03-10 14:46:44 +0700

    redo_3

---
+++ redo_3.py
@@ -0,0 +1,17 @@
+
+A = []
+for item in range (1,21):
+    A.append(item)
+    
+for i in A:    
+    if A[i-1] % 2 == 0:
+        print('genap')
+    else:
+        print('ganjil')
+        
+    
+        
+
+        
+
+    
