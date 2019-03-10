4213a434571f00fc013787911b95c7f4493018be
Redo Faris Novrizki <redofaris@gmail.com>
2019-03-10 16:07:53 +0700

    redo_4

---
+++ redo_4.py
@@ -0,0 +1,18 @@
+def printprime(A):
+    for angka in A:
+        isi = 0
+        for i in range(1,angka+1):
+            if angka %1+1:
+                isi += 1
+            if isi ==2:
+                print('bilangan prima')
+            elif isi == 1:
+                print('bilangan prima')
+            else:
+                print('bukan bilangan prima)
+            isi = 0
+        
+
+PrintPrime([1,2,3,4,5,6,7,8,9,10])
+        
+   
