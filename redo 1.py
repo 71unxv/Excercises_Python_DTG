05c2b1811ad157ded4572ab0057a824f416bbfae
Redo Faris Novrizki <redofaris@gmail.com>
2019-03-10 13:06:38 +0700

    redo_1

---
+++ redo 1.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 10 12:52:16 2019
+
+@author: redof
+"""
+
+a = 8
+b = 6
+c = 12
+d = 1
+p1 = 100.00
+p2 = 75.00
+p3 = 150.00
+p4 = 12.50
+
+print(f"john have {a} apple the price is{p1}")
+print(f"dilan have {b} apple the price is {p2}")
+print(f"sarah have {c} apple the price is {p3}")
+print(f"a price for {d} apple is {p4}")
