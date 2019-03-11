a059d4120689b068266a5c32ed4a9ae777ee2668
rintabt <rintabt@gmail.com>
2019-03-11 09:48:23 +0700

    matriks ganj gen_02

--- rinta_9.py
+++ rinta_9.py
@@ -12,9 +12,9 @@ s = (9,9)
 S = np.zeros(s)
 
 for x in range (1,9):
-    if (x-1)%2==1:
+    if x%2==1:
         for y in range (1,9):
-            if (y-1)%2==1:
-                S[x-1][y-1]=1
+            if y%2==1:
+                S[x][y]=1
         
 print(S)
